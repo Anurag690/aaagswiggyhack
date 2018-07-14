1364478ec1d885a0766e235edfcf68cf2d55f7a8
gauravprwl14 <gauravprwl14@gmail.com>
2018-07-15 05:19:09 +0530

    added the UI component for the main page

--- src/components/routes/mainPage/index.js
+++ src/components/routes/mainPage/index.js
@@ -1,8 +1,60 @@
 import React, { Component } from "react";
+import {
+  Panel,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Radio,
+  Button
+} from "react-bootstrap";
+
+import "./styles.css";
 
 class MainPage extends Component {
   render() {
-    return <div> FIrst Page </div>;
+    return (
+      <div className="app-container">
+        <div className="container">
+          <div className="main-page-container">
+            <div className="main-page-wrapper">
+              <div className="main-page-title">
+                <h1>Swiggy Hackathon</h1>
+              </div>
+              <div className="main-page-form">
+                <Panel bsStyle="primary">
+                  <Panel.Heading>Input Form</Panel.Heading>
+                  <Panel.Body>
+                    <FormGroup controlId="formBasicText">
+                      <ControlLabel>Geo Location</ControlLabel>
+                      <FormControl
+                        type="text"
+                        placeholder="Enter Geo Location"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <ControlLabel>Filter Type</ControlLabel> &nbsp;
+                      <Radio name="filterType" inline>
+                        Category
+                      </Radio>{" "}
+                      <Radio name="filterType" inline>
+                        Item
+                      </Radio>{" "}
+                    </FormGroup>
+                    <FormGroup controlId="formBasicText">
+                      <ControlLabel>User Id</ControlLabel>
+                      <FormControl type="text" placeholder="Enter User Id" />
+                    </FormGroup>
+                  </Panel.Body>
+                  <Panel.Footer className="text-center">
+                    <Button bsStyle="success"> Submit </Button>
+                  </Panel.Footer>
+                </Panel>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
