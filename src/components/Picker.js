ba02402a7f66a723a881757ae26aa115bf85f975
gauravprwl14 <gauravprwl14@gmail.com>
2018-07-15 04:19:52 +0530

    modified the picker and map component

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import MyMapComponent from './map';
-
+import MyMapComponent from "./map";
 
 const Picker = ({ value, onChange, options }) => (
-  <div style={{display: 'flex', flexDirection: 'row', width: '80%'}}>
+  <div style={{ display: "flex", flexDirection: "row", width: "80%" }}>
     <MyMapComponent
       isMarkerShown
       googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
@@ -14,14 +13,12 @@ const Picker = ({ value, onChange, options }) => (
       mapElement={<div style={{ height: `100%` }} />}
     />
   </div>
-)
+);
 
 Picker.propTypes = {
-  options: PropTypes.arrayOf(
-    PropTypes.string.isRequired
-  ).isRequired,
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired
-}
+};
 
-export default Picker
+export default Picker;
