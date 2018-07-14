357d73a3166dbcd670fbe90e535ae695a2903ba8
gauravprwl14 <gauravprwl14@gmail.com>
2018-07-15 04:11:22 +0530

    added the store folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { selectSubreddit, fetchPostsIfNeeded, invalidateSubreddit } from '../actions'
-import Picker from '../components/Picker'
-import Placard from '../components/placard';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  selectSubreddit,
+  fetchPostsIfNeeded,
+  invalidateSubreddit
+} from "../store/actions";
+import Picker from "../components/Picker";
+import Placard from "../components/placard";
 
 class App extends Component {
   static propTypes = {
@@ -12,74 +16,89 @@ class App extends Component {
     isFetching: PropTypes.bool.isRequired,
     lastUpdated: PropTypes.number,
     dispatch: PropTypes.func.isRequired
-  }
+  };
 
   componentDidMount() {
-    const { dispatch, selectedSubreddit } = this.props
-    dispatch(fetchPostsIfNeeded(selectedSubreddit))
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.selectedSubreddit !== this.props.selectedSubreddit) {
-      const { dispatch, selectedSubreddit } = nextProps
-      dispatch(fetchPostsIfNeeded(selectedSubreddit))
+      const { dispatch, selectedSubreddit } = nextProps;
+      dispatch(fetchPostsIfNeeded(selectedSubreddit));
     }
   }
 
   handleChange = nextSubreddit => {
-    this.props.dispatch(selectSubreddit(nextSubreddit))
-  }
+    this.props.dispatch(selectSubreddit(nextSubreddit));
+  };
 
   handleRefreshClick = e => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const { dispatch, selectedSubreddit } = this.props
-    dispatch(invalidateSubreddit(selectedSubreddit))
-    dispatch(fetchPostsIfNeeded(selectedSubreddit))
-  }
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(invalidateSubreddit(selectedSubreddit));
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
+  };
 
   render() {
-    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props
-    const isEmpty = posts.length === 0
+    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props;
+    const isEmpty = posts.length === 0;
     return (
-      <div className="hide-scrollbar-from-window" style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
-        <Picker value={selectedSubreddit}
-                onChange={this.handleChange}
-                options={[ 'reactjs', 'frontend' ]} /> 
-        {isEmpty
-          ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
-          : <div style={{ opacity: isFetching ? 0.5 : 1, overflowY: 'scroll', height: window.innerHeight, width: '-webkit-fill-available'  }}>
-              <Placard/>
-              <Placard/>
-              <Placard/>
-              <Placard/>
-              <Placard/>
-              <Placard/>
-              <Placard/> 
-            </div>
-        }
+      <div
+        className="hide-scrollbar-from-window"
+        style={{ display: "flex", flexDirection: "row", width: "100%" }}
+      >
+        <Picker
+          value={selectedSubreddit}
+          onChange={this.handleChange}
+          options={["reactjs", "frontend"]}
+        />
+        {isEmpty ? (
+          isFetching ? (
+            <h2>Loading...</h2>
+          ) : (
+            <h2>Empty.</h2>
+          )
+        ) : (
+          <div
+            style={{
+              opacity: isFetching ? 0.5 : 1,
+              overflowY: "scroll",
+              height: window.innerHeight,
+              width: "-webkit-fill-available"
+            }}
+          >
+            <Placard />
+            <Placard />
+            <Placard />
+            <Placard />
+            <Placard />
+            <Placard />
+            <Placard />
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
-  const { selectedSubreddit, postsBySubreddit } = state
-  const {
-    isFetching,
-    lastUpdated,
-    items: posts
-  } = postsBySubreddit[selectedSubreddit] || {
+  const { selectedSubreddit, postsBySubreddit } = state;
+  const { isFetching, lastUpdated, items: posts } = postsBySubreddit[
+    selectedSubreddit
+  ] || {
     isFetching: true,
     items: []
-  }
+  };
 
   return {
     selectedSubreddit,
     posts,
     isFetching,
     lastUpdated
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
